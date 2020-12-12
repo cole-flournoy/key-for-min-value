1453e014e1cc049364386817b2823a4beb8433f8
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-11 22:37:33 -0500

    commit tree 53b2f8cacedc15ed7b796ab7f2cc510c57cbfcb6

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    return nil if name_hash == {}
+    all_values = name_hash.collect {|key, value| value}    
+    lowest = all_values[0]
+    sorted_values = all_values.collect do |number|
+        lowest = number if number < lowest
+    end
+    name_hash.select do |key, value| 
+        return key if value == lowest
+    end
 end
